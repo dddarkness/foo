bb91092a7c760c109770602789f7f369235df7f7
dddarkness <saara.sarvipaa@outlook.com>
2020-08-31 20:13:09 +0300

    foo

--- vcf/vc.sh
+++ vcf/vc.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-if [[ ! -d pops/ ]]; then
+set -e
+
+hgdp_files='../../hgdp_wgs/hgdp*chr*vcf.gz'
+popsdir='../pops/'
+workdir=work-$$
+
+mkdir $workdir
+cd $workdir
+
+if [[ ! -d ${popsdir} ]]; then
 	echo Put population .txt files to dir pops/
 	exit 1
 fi
 
-for f in $(ls *hgdp*chr*vcf.gz); do
+for f in $(ls $hgdp_files); do
 	chr=$(echo $f | sed -e 's/.*chr//' -e 's/\..*//')
-	for pop in $(ls pops/*.txt); do
+	echo chr $chr
+	for pop in $(ls ${popsdir}/*.txt); do
 		popname=$(echo $(basename $pop) | sed -e 's/.txt$//')
 		echo processing: vcftools --gzvcf $f --keep ${pop} --freq --out ${popname}-Chr${chr}freq
 		vcftools --gzvcf $f --keep ${pop} --freq --out ${popname}-Chr${chr}freq
 	done
 done
+
+cd -
