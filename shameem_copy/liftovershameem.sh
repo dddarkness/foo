bf66d7bfe2aa7b7e048903375c6aac1b77fc9701
dddarkness <saara.sarvipaa@outlook.com>
2020-08-31 20:59:33 +0300

    foo

--- shameem_copy/liftovershameem.sh
+++ shameem_copy/liftovershameem.sh
@@ -4,6 +4,7 @@ set -e
 
 # paths:
 plink='../../plink'
+rmBadLifts='../rmBadLifts.py'
 vcf='../../hgdp_wgs'
 workdir=work-$$
 
@@ -17,15 +18,17 @@ function convert_BED_to_PED()
 
 function find_unlifted_SNPs_from_PED()
 {
-	../liftOverPlink.py --map ../../vcf/chr21.map --out lifted --chain ../GRCh38_to_GRCh37.chain -e ../liftOver
+	chr=$1
+	../liftOverPlink.py --map hgdp_${chr}.map --out lifted --chain ../GRCh38_to_GRCh37.chain -e ../liftOver
 }
 
 function remove_bad_lifted_SNPs()
 {
-	../rmBadLifts.py --map lifted.map --out good_lifted.map --log bad_lifted.dat
+	chr=$1
+	$rmBadLifts --map lifted.map --out good_lifted.map --log bad_lifted.dat
 	cut -f 2 bad_lifted.dat > to_exclude.dat
 	cut -f 4 lifted.bed.unlifted | sed "/^#/d" >> to_exclude.dat
-	$plink --file ../../vcf/chr21.plink --recode --out lifted --exclude to_exclude.dat
+	$plink --file hgdp_${chr} --recode --out lifted --exclude to_exclude.dat
 }
 
 function make_final_output_PED()
@@ -35,18 +38,26 @@ function make_final_output_PED()
 
 function convert_PED_to_BED()
 {
-	echo
+	echo TODO
 }
 
 function main()
 {
+	chr=chr${1}
 	mkdir $workdir
 	cd $workdir
 	
-	convert_BED_to_PED chr21
+	convert_BED_to_PED $chr
+	find_unlifted_SNPs_from_PED $chr
+	remove_bad_lifted_SNPs $chr
+	make_final_output_PED $chr
 
 	cd -
 }
 
-main $*
+if [[ $# -eq 1 ]]; then
+	main $*
+else
+	echo usage: $(basename $0) '<chr number>'
+fi
 
