3fc5978d07ad70164c31aa7fe86a642260fc97f7
dddarkness <saara.sarvipaa@outlook.com>
2020-09-02 19:34:19 +0300

    foo

--- shameem_copy/doit.sh
+++ shameem_copy/doit.sh
@@ -7,15 +7,16 @@ rmBadLifts='../rmBadLifts.py'
 vcf='../../hgdp_wgs'
 pops=../../vcf/pops
 ea_mtag_snips='../EA_MTAG_SNPs.txt'
-# workdir=work-$$
-workdir=work-1371
+workdir=work-$$
+# workdir=work-1371
 
 #  in: vcf
 # out: .ped and .map
-function convert_BED_to_PED()
+function vcf_to_ped_and_map()
 {
 	chr=$1
-        $plink --vcf $vcf/hgdp_wgs*${chr}*vcf.gz --recode --tab --out hgdp_${chr}
+	bcftools view -k $vcf/hgdp_wgs*${chr}*vcf.gz -o hgdp_${chr}.vcf
+        $plink --vcf $vcf/hgdp_wgs*${chr}*vcf --recode --tab --out hgdp_${chr}
 }
 
 function find_unlifted_SNPs_from_PED()
@@ -49,8 +50,15 @@ function do_it_without_lifting()
 	chrnum=$(echo $chr | sed -e 's/chr//')
 	pop=$2
 	popname=$(basename $pop | sed -e 's/.txt$//')
-	$plink --file hgdp_${chr} --extract $ea_mtag_snips --keep-fam $pop --freq --make-bed --out ${popname}${chrnum}freq
+	$plink --file merged.out --extract $ea_mtag_snips --keep-fam $pop --freq --make-bed --out ${popname}${chrnum}freq
 }
+
+function merge()
+{
+	ls hgdp_*.map | xargs -l | sed -e 's/.*\./&ped &/' > merge.list
+	$plink --merge-list merge.list --recode --out merged.out
+}
+
 
 function main()
 {
@@ -60,21 +68,21 @@ function main()
 	cd $workdir
 
 	for c in $(ls $vcf/hgdp_wgs*${chr}*vcf.gz); do
-
 		chrnum=$(echo $c | sed -e 's/.*chr//' -e 's/.vcf.gz//')
 		chr=chr${chrnum}
 
 		echo processing:  $chr
 
-		convert_BED_to_PED $chr
-		find_unlifted_SNPs_from_PED $chr
+		vcf_to_ped_and_map $chr
+		# find_unlifted_SNPs_from_PED $chr
 		# remove_bad_lifted_SNPs $chr
 		# make_final_output_PED $chr
+	done
 
-		for pop in $(ls $pops/*.txt); do
-			do_it_without_lifting $chr $pop
-		done
+	merge
 
+	for pop in $(ls $pops/*.txt); do
+		do_it_without_lifting $chr $pop
 	done
 
 	cd -
