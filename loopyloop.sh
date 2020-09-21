041666411b88359373a12fa35f742f4a55198067
dddarkness <saara.sarvipaa@outlook.com>
2020-09-21 17:24:39 +0300

    fix

--- loopyloop.sh
+++ loopyloop.sh
@@ -5,7 +5,8 @@ do
 	for indvrow in $(grep 'HGDP' $popfile | sed -e 's/.$//');
 	do
 		for pop in $(ls vcf/pops/*.txt);
-			do echo vcftools --indv $indvrow --vcf total_chroms.vcf --snps EA_MTAG_SNPs.txt --keep $pop --freq --out freq-$(basename ${pop})
+			do echo vcftools --indv $indvrow --vcf total_chroms.vcf --snps EA_MTAG_SNPs.txt --freq --out freq-${indvrow}-$(basename ${pop})
+			#do echo vcftools --indv $indvrow --vcf total_chroms.vcf --snps EA_MTAG_SNPs.txt --keep $pop --freq --out freq-${indvrow}-$(basename ${pop})
 		done
 	done
 done
